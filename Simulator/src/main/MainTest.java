f312f702698f3beed25282ed41d9359b0d262189
Dulioust Magali <magali.dulioust@telecom-paristech.fr>
2015-06-16 16:15:16 +0200

    creation loi de poisson et main de test

---
+++ Simulator/src/main/MainTest.java
@@ -0,0 +1,16 @@
+package main;
+
+import alea.Alea;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0; i<100; i++){
+			
+			System.out.println(Alea.poisson(2));
+		}
+	}
+
+}
