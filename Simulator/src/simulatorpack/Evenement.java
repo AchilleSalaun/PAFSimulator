d6a97c53b91892e29fa822484717bd389a90cd1f
Ariel Haziza <ar.haziza@hotmail.fr>
2015-06-16 16:22:48 +0200

    Implémentation de la classe Evenement

--- Simulator/src/simulatorpack/Evenement.java
+++ Simulator/src/simulatorpack/Evenement.java
@@ -1,5 +1,33 @@
 package simulatorpack;
 
-public class Evenement {
+import java.sql.Date;
 
+public class Evenement {
+    
+    
+    public class Evenement {
+        
+        private Objet acteur;
+        private Date temps ;
+        private String action ;
+        
+        
+        
+        public Objet getActeur(){
+            return this.acteur;
+        }
+        
+        
+        public Date getDate(){
+            return this.temps;
+        }
+        
+        
+        public String getAction(){
+            return this.action;
+        }
+        
+        
+        
+    }
 }
