37d4367b1dffd6476c01769e06270c8dd7c6675f
francklin <joe.handoukemayou@yahoo.fr>
2015-06-16 16:06:09 +0200

    Creation du package SimulatorPack. Creation des classes Echeanciers et Evenement.

---
+++ Simulator/src/simulatorpack/Echeancier.java
@@ -0,0 +1,9 @@
+package simulatorpack;
+
+
+import java.util.LinkedList;
+
+import modele.Client;
+public class Echeancier {
+
+}
---
+++ Simulator/src/simulatorpack/Evenement.java
@@ -0,0 +1,5 @@
+package simulatorpack;
+
+public class Evenement {
+
+}
