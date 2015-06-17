afd9229cf33fd4373628eb4b7771a1018cfe7b50
Magali Dulioust <magali.dulioust@telecom-paristech.fr>
2015-06-17 10:06:12 +0200

    Creation d'un comparateur d'evenements, construction de l'echeancier avec une priority queue et petites modifications d'alea

--- Simulator/src/simulatorpack/Echeancier.java
+++ Simulator/src/simulatorpack/Echeancier.java
@@ -1,9 +1,22 @@
 package simulatorpack;
 
 
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 import modele.Client;
-public class Echeancier {
 
+public class Echeancier extends PriorityQueue<Evenement>{
+
+	private static final long serialVersionUID = 1L;
+	
+	static EventComparator comparator = new EventComparator();
+	
+	public Echeancier(){
+		super(comparator);
+	}
+	
+
+	
 }
