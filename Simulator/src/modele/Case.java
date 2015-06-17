e37630b097fabf8e674516ab915b271c10940126
francklin <joe.handoukemayou@yahoo.fr>
2015-06-17 12:48:02 +0200

    getteurs et setteurs pour les attributs entree et sorties

--- Simulator/src/modele/Case.java
+++ Simulator/src/modele/Case.java
@@ -1,10 +1,25 @@
 package modele;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public abstract class Case {
-	private PriorityQueue listeObjets;
+	private PriorityQueue<Objet> listeObjets;
+    private ArrayList <Case> entree;
+    private ArrayList <Case> sortie;
     
-	
-	
+    public ArrayList <Case> getEntree(){
+    	return this.entree;
+    }
+    
+    public ArrayList <Case> getSortie() {
+    	return this.sortie;
+    }
+	public void setEntree(ArrayList<Case> entree) {
+		this.entree = entree;
+	}
+	public void setSortie(ArrayList<Case> sortie){
+		this.sortie = sortie;
+	}
 }
