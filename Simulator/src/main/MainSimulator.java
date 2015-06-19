021ffcc6becd9f9e1c914cf73c5b09cc113ac495
Magali Dulioust <magali.dulioust@telecom-paristech.fr>
2015-06-19 14:26:56 +0200

    début de mainSimulator, mise en place de la boucle

--- Simulator/src/main/MainSimulator.java
+++ Simulator/src/main/MainSimulator.java
@@ -1,10 +1,17 @@
 package main;
 
+import simulatorpack.Echeancier;
+
 public class MainSimulator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Echeancier echeancier = new Echeancier();
+		
+		while(!echeancier.isEmpty()){
+			echeancier.nextEvent();
+		}
 	}
 
 }
